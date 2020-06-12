f63a5f15b26e6a8ebe6b71b4995f78b60cd58cdf
MikeLarah <mike.larah@endjin.com>
2020-06-12 12:54:06 +0100

    Updates to deployment project (#107)
    * WIP adding Marain.Services.Tenancy components to claims.
    Tests are passing. Need to check the functions host works correctly.
    
    * Update pipeline
    
    * Update service collection extensions for functions host
    
    * Updating deployment project to be compliant with Marain.Instance deployment
    
    * Update pipeline to archive artefacts
    
    * Fix path for deployment project
    
    * Fix functions app deploy name
    
    * Remove old reference to app-insights resource
    
    * Fix reference to functions deploy resource
    
    * Add empty app suffix to appease the method signature
    
    * Add example config for using key vault to get storage account key

---
+++ Solutions/Marain.Claims.Deployment/Marain-PostDeployAad.ps1
@@ -0,0 +1,18 @@
+﻿<#
+This is called during Marain.Instance infrastructure deployment after the Marain-ArmDeploy.ps
+script. It is our opportunity to do any deployment work that needs to happen after Azure resources
+have been deployed.
+#>
+
+# Marain.Instance expects us to define just this one function.
+Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
+
+    Write-Host "Assigning application service principals to the tenancy service app's reader role"
+
+    $TenancyReaderAppRoleId = "60743a6a-63b6-42e5-a464-a08698a0e9ed"
+    $ServiceDeploymentContext.AssignServicePrincipalToCommonServiceAppRole(
+        "Marain.Tenancy",
+        $TenancyReaderAppRoleId,
+        ""
+    )
+}
---
+++ Solutions/Marain.Claims.Deployment/Marain-PreDeploy.ps1
@@ -0,0 +1,24 @@
+﻿<#
+This is called during Marain.Instance infrastructure deployment prior to the Marain-ArmDeploy.ps
+script. It is our opportunity to perform initialization that needs to complete before any Azure
+resources are created.
+
+We create the Azure AD Applications that the Workflow functions will use to authenticate incoming
+requests. (Currently, this application is used with Azure Easy Auth, but the service could also
+use it directly.)
+
+#>
+
+# Marain.Instance expects us to define just this one function.
+Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
+
+    $App = $ServiceDeploymentContext.DefineAzureAdAppForAppService()
+
+    $AdminAppRoleId = "7619c293-764c-437b-9a8e-698a26250efd"
+    $App.EnsureAppRolesContain(
+        $AdminAppRoleId,
+        "Claims administrator",
+        "Full control over definition of claim permissions and rule sets",
+        "ClaimsAdministrator",
+        ("User", "Application"))
+}
