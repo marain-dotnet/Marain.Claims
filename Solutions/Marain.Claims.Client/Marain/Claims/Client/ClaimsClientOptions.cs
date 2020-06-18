fc431085ffe7bfad1b15b74f4a50264bba675d27
MikeLarah <mike.larah@endjin.com>
2020-06-18 10:27:08 +0100

    Update the service collection extensions for adding ClaimsClient. - Added ClaimsClientOptions type - Uses a callback for options instead of passing them in directly - Added method for initializing claims client with caching

---
+++ Solutions/Marain.Claims.Client/Marain/Claims/Client/ClaimsClientOptions.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="MarainClaimsClientOptions.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.Claims.Client
+{
+    using System;
+
+    /// <summary>
+    /// Options for configuring <see cref="ClaimsService"/> via <see cref="ClaimsClientServiceCollectionExtensions"/>
+    /// </summary>
+    public class ClaimsClientOptions
+    {
+        /// <summary>
+        /// Gets or sets the base URI for the Claims service
+        /// </summary>
+        public Uri BaseUri { get; set; }
+
+        /// <summary>
+        /// Gets or sets the resource ID of the AAD app used for the easyAuth for
+        /// the claims service.
+        /// </summary>
+        public string ResourceIdForMsiAuthentication { get; set; }
+    }
+}
