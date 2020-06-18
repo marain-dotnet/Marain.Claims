91c2acf5c1b96d5cd0de3869a8df76695599b239
MikeLarah <mike.larah@endjin.com>
2020-06-18 15:36:20 +0100

    Update the service collection extensions for adding ClaimsClient. (#112)
    * Update the service collection extensions for adding ClaimsClient.
    - Added ClaimsClientOptions type
    - Uses a callback for options instead of passing them in directly
    - Added method for initializing claims client with caching
    
    * Actually use the cache options and made it configurable

---
+++ Solutions/Marain.Claims.Client/Marain/Claims/Client/ClaimsClientOptions.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="ClaimsClientOptions.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.Claims.Client
+{
+    using System;
+
+    /// <summary>
+    /// Options for configuring <see cref="ClaimsService"/> via <see cref="ClaimsClientServiceCollectionExtensions.AddClaimsClient(Microsoft.Extensions.DependencyInjection.IServiceCollection, System.Func{System.IServiceProvider, ClaimsClientOptions})"/>
+    /// </summary>
+    public class ClaimsClientOptions
+    {
+        /// <summary>
+        /// Gets or sets the base URI for the Claims service
+        /// </summary>
+        public Uri BaseUri { get; set; }
+
+        /// <summary>
+        /// Gets or sets the resource ID of the AAD app used for the easyAuth for
+        /// the claims service.
+        /// </summary>
+        public string ResourceIdForMsiAuthentication { get; set; }
+    }
+}
---
+++ Solutions/Marain.Claims.Client/Marain/Claims/Client/ClaimsClientWithCachingOptions.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="ClaimsClientWithCachingOptions.cs" company="Endjin">
+// Copyright (c) Endjin. All rights reserved.
+// </copyright>
+
+namespace Marain.Claims.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+
+    /// <summary>
+    /// Options for configuring <see cref="ClaimsService"/> via <see cref="ClaimsClientServiceCollectionExtensions.AddClaimsClientWithCaching(Microsoft.Extensions.DependencyInjection.IServiceCollection, System.Func{System.IServiceProvider, ClaimsClientWithCachingOptions})"/>
+    /// </summary>
+    public class ClaimsClientWithCachingOptions : ClaimsClientOptions
+    {
+        /// <summary>
+        /// Gets or sets the dictionary containing mappings of HTTP status codes to cache expiration periods.
+        /// </summary>
+        public Dictionary<HttpStatusCode, TimeSpan> CacheExpirationPerHttpResponseCode { get; set; }
+    }
+}
