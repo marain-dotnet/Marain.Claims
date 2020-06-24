48285c8cbb3ecab22180413a52e4f7e4bab3a61f
MikeLarah <mike.larah@endjin.com>
2020-06-24 15:19:27 +0100

    Add 'client' role to app roles for claims app

--- Solutions/Marain.Claims.Deployment/Marain-PreDeploy.ps1
+++ Solutions/Marain.Claims.Deployment/Marain-PreDeploy.ps1
@@ -3,7 +3,7 @@ This is called during Marain.Instance infrastructure deployment prior to the Mar
 script. It is our opportunity to perform initialization that needs to complete before any Azure
 resources are created.
 
-We create the Azure AD Applications that the Workflow functions will use to authenticate incoming
+We create the Azure AD Applications that the Claims functions will use to authenticate incoming
 requests. (Currently, this application is used with Azure Easy Auth, but the service could also
 use it directly.)
 
@@ -15,10 +15,19 @@ Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentCon
     $App = $ServiceDeploymentContext.DefineAzureAdAppForAppService()
 
     $AdminAppRoleId = "7619c293-764c-437b-9a8e-698a26250efd"
+    $ClientAppRoleId = "be84cc43-d5fb-4e37-91f2-1d46fc353c69"
+
     $App.EnsureAppRolesContain(
         $AdminAppRoleId,
         "Claims administrator",
         "Full control over definition of claim permissions and rule sets",
         "ClaimsAdministrator",
+        ("User", "Application"))
+
+    $App.EnsureAppRolesContain(
+        $ClientAppRoleId,
+        "Client",
+        "Can evaluate claim permissions in the Claims service",
+        "Client",
         ("User", "Application"))
 }
