98ca775aa916ccdc6496811013756a26fe25fca4
Ian Griffiths <Ian.Griffiths@endjin.com>
2021-11-15 08:48:09 +0000

    Take extra test build step back out
    Tests now working so we can live without this
    
    Also try fixing line that sets build number in AzDevOps

--- build.ps1
+++ build.ps1
@@ -133,14 +133,15 @@ task . FullBuild
 
 # build extensibility tasks
 task PostVersion {
-    # Should be these, but Endjin.RecommendePractices.Build beta008 is not adding the GITVERSION_ prefix
+    # Should be these, but Endjin.RecommendedPractices.Build beta008 is not adding the GITVERSION_ prefix
     # Write-Host "##vso[task.setvariable variable=Endjin_IsPreRelease]$((-not ([string]::IsNullOrEmpty($Env:GITVERSION_PRERELEASETAG))))"
     # Write-Host "##vso[task.setvariable variable=Endjin_Repository_Name]$Env:BUILD_REPOSITORY_NAME"
     Write-Host "##vso[task.setvariable variable=Endjin_IsPreRelease]$((-not ([string]::IsNullOrEmpty($Env:PRERELEASETAG))))"
     Write-Host "##vso[task.setvariable variable=Endjin_Repository_Name]$Env:BUILD_REPOSITORY_NAME"
 
-    # Scripted build currently doesn't do this for us
-    Write-Host "##vso[build.updatebuildnumber]]$Env:BUILD_REPOSITORY_NAME"
+    # Scripted build currently doesn't do this for us.
+    # This should really be $Env:GITVERSION_SemVer, but see comment above re beta008 bug.
+    Write-Host "##vso[build.updatebuildnumber]]$Env:SemVer"
 }
 task PreBuild {}
 task PostBuild {}
